fa3b97a9256523c1b4dcf817a5aec7199668ebbe
Nitzan Elbaz <nitzan@claysciences.com>
2018-04-05 13:40:28 +0300

    Silence errors

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # Remove current configuration and create links to new configs
 # Old configuration is backed up.
 # untested
-mv ~/.bashrc ~/.bashrc_backup
-mv ~/.vimrc ~/.vimrc_backup
-mv ~/.vim ~/.vim_backup
-mv ~/.tmux.conf ~/.tmux.conf_backup
+mv ~/.bashrc ~/.bashrc_backup 2 > /dev/null
+mv ~/.vimrc ~/.vimrc_backup 2>/dev/null
+mv ~/.vim ~/.vim_backup 2>/dev/null
+mv ~/.tmux.conf ~/.tmux.conf_backup 2>/dev/null
 ln -s vimrc ~/.vimrc
 tar -xf vim.tar vim
 ln -s vim ~/.vim
