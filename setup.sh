a208b52d69c1fad1899a6a507c5759a07abafffd
Nitzanel <nitzan@claysciences.com>
2018-04-05 16:43:40 +0300

    setup script is now tested

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 # Remove current configuration and create links to new configs
 # Old configuration is backed up.
-# untested
 mv ~/.bashrc ~/.bashrc_backup 2>/dev/null
 mv ~/.vimrc ~/.vimrc_backup 2>/dev/null
 mv ~/.vim ~/.vim_backup 2>/dev/null
